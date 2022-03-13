1cde439813940911a275f942d6197208c5d07cf1
Richard <skynet.tw@gmail.com>
2022-03-13 21:47:14 +0800

    加入專案檔案。

---
+++ mytest/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mytest
+{
+    class Person
+    {
+        private string name;
+        private double height;
+        private double weight;
+        private int age;
+        public static int count = 0;
+
+        public Person(string n, double h, double w, int a)
+        {
+            this.name = n;
+            this.height = h;
+            this.weight = w;
+            this.Age = a;
+            count++;
+        }
+
+        public int Age
+        {
+            get { return age;  }
+            set {
+                this.age = value;
+            }
+        }
+    }
+}
---
+++ mytest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace mytest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] d = {
+                { 1, 2, 3},
+                { 4, 5, 6},
+                { 7, 8, 9}
+            };
+            Console.WriteLine(d.Length);
+            Person richard = new Person("Richard", 1.74, 70.0, 20);
+            Person tom = new Person("Tom", 1.74, 70.0, 20);
+            richard.Age = 50;
+            Console.WriteLine(richard.Age);
+            Console.WriteLine(Person.count);
+        }
+    }
+}
